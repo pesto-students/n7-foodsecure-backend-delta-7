6d46962c9eebf5ac4cc54b2ea686acebe63df177
Gurdeep <gs22111994@gmail.com>
2021-10-10 05:38:14 -0400

    update query for lat lng

--- src/modules/ngo_pickup/ngo_pickup.service.ts
+++ src/modules/ngo_pickup/ngo_pickup.service.ts
@@ -21,7 +21,7 @@ export class NGOPickupService {
 
   async findPendingPickupRequests(latLng: any, status: string): Promise<any> {
     const { lat_min, lat_max, lng_min, lng_max } = latLng;
-    const query = `select b.id as pickupId, a.name, a.email, b.price, b.prepared_time, b.expiry_time, b.number_of_meals FROM public."Users" as a Inner join public."PickupRequests" as b on b."userId" = a.id where a."lat" >=${lat_min} and a."lng" <= ${lat_max} and a."lng" >=${lng_min} and a."lng"<=${lng_max} and role = 'restaurant' and status='${status}'`;
+    const query = `select b.id as pickupId, a.name, a.email, b.price, b.prepared_time, b.expiry_time, b.number_of_meals FROM public."Users" as a Inner join public."PickupRequests" as b on b."userId" = a.id where a."lat" >=${lat_min} and a."lat" <= ${lat_max} and a."lng" >=${lng_min} and a."lng"<=${lng_max} and role = 'restaurant' and status='${status}'`;
     console.log('NGO Query : ' + query);
     const db = await databaseProviders[0].useFactory();
     const result = await db.query(query);
