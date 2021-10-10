523818f6ef39f27cd5edd21386702a77f68c7b84
Gurdeep <gs22111994@gmail.com>
2021-10-09 20:48:07 -0400

    add pickup id

--- src/modules/ngo_pickup/ngo_pickup.service.ts
+++ src/modules/ngo_pickup/ngo_pickup.service.ts
@@ -21,7 +21,7 @@ export class NGOPickupService {
 
   async findPendingPickupRequests(latLng: any, status: string): Promise<any> {
     const { lat_min, lat_max, lng_min, lng_max } = latLng;
-    const query = `select a.email, b.price, b.prepared_time, b.expiry_time, b.number_of_meals FROM public."Users" as a Inner join public."PickupRequests" as b
+    const query = `select b.id as pickupId, a.email, b.price, b.prepared_time, b.expiry_time, b.number_of_meals FROM public."Users" as a Inner join public."PickupRequests" as b
    on b."userId" = a.id
    where a."lat" >=${lat_min}
    and a."lng" <= ${lat_max} and 
